94146b6e541fa9c6a8ee9f7fdd1ab18c2eee0aa0
lorem <79251934+adisovaa@users.noreply.github.com>
2023-03-10 23:06:14 -0500

    new kata

---
+++  Opposites Attract.js
@@ -0,0 +1,14 @@
+// function lovefunc(flower1, flower2){
+//    if ((flower1 + flower2) % 2 ) {
+//      return true
+//    } else {
+//     return false
+//    }
+// }
+
+function lovefunc(flower1, flower2){
+    return ((flower1 + flower2) % 2 ) ? true : false
+    }
+
+console.log((lovefunc(1,4)))
+console.log((lovefunc(2,4)))
