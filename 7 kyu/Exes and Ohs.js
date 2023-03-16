1d060cca93ad2a9b6f68eea7ccdaffc87fa4a89d
lorem <79251934+adisovaa@users.noreply.github.com>
2023-03-15 23:03:16 -0400

    new changes

---
+++ 7 kyu/Exes and Ohs.js
@@ -0,0 +1,24 @@
+// 1
+function XO(str) {
+    let arr = str.toLowerCase().split('');
+    return arr.filter(element => element == 'x').length == arr.filter(element => element == 'o').length;
+}
+
+
+//  2
+function XO(str) {
+    var sum = 0;
+    for(var i=0; i<str.length; i++){
+      if(str[i].toLowerCase() == 'x') sum++;
+      if(str[i].toLowerCase() == 'o') sum--;
+    }
+    return sum == 0;
+}
+
+
+// 3
+function XO(str) {
+    let x = str.match(/x/gi);
+    let o = str.match(/o/gi);
+    return (x && x.length) === (o && o.length);
+}
