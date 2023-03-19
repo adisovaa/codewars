0ae162f8fd34e61a988e66c695cef220f386b2d5
lorem <79251934+adisovaa@users.noreply.github.com>
2023-03-18 22:22:32 -0400

    new changes

---
+++ 8 kyu/Function 1 - hello world.js
@@ -0,0 +1,5 @@
+// Write a function "greet" that returns "hello world!"
+
+const greet = () => {
+    return 'hello world!'
+}
--- 8 kyu/Total amount of points.js
+++ 8 kyu/Total amount of points.js
@@ -7,4 +7,13 @@ function points(games) {
         s += 1;
     }
     return s;
-  }
+}
+
+
+
+// function points(games) {
+//     return games.reduce((current, element) => {
+//         let arraySplit = element.split(':');
+//         return (arraySplit[0] > arraySplit[1]) ? current += 3 : (arraySplit[0] < arraySplit[1]) ? current : current += 1;
+//     }, 0);
+// }
